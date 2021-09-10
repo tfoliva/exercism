a3cc1d3bc5ef5e0fedb2f396ed81bbadf6644e08
Aocci <tiago.oliva.costa@gmail.com>
2021-09-10 18:42:11 -0300

    refactor: change numbers to letters for clarity

--- c/isogram/isogram.c
+++ c/isogram/isogram.c
@@ -1,13 +1,11 @@
 #include "isogram.h"
 
 bool repeated_char(const char* letters, const char x) {
-        int rep_count = 0;
-        while (*letters) {
-                if (x == *letters) {
-                        rep_count++;
-                }
-                letters++;
-        }
+        int  rep_count = 0;
+        char c;
+
+        while ((c = *letters++))
+                if (x == c) rep_count++;
 
         return rep_count;
 }
@@ -15,30 +13,24 @@ bool repeated_char(const char* letters, const char x) {
 bool is_isogram(const char* phrase) {
         if (!phrase) return false;  // if pointer is NULL or 0
 
+        char p           = 0;
         char letters[26] = {0};  // at max, phrase could contain the whole
                                  // alphabet without repetition
 
-        char p = 0;
-        for (int i = 0; *phrase; ++i) {
-                p = *phrase;
-
-                if (p < 65) {
-                        ++phrase;  // ignore hyphens
+        for (int i = 0; (p = *phrase++); ++i) {
+                if (p < 'A') {
                         letters[i] = -1;
                         continue;
                 }
 
                 // guarantee that all letters are lowercase
-                if (p >= 65 && p <= 90) {  // if uppercase
-                        p = *phrase + 32;  // convert to lowercase
-                }
+                if (p >= 'A' && p <= 'Z')  // if uppercase
+                        p += 32;           // convert to lowercase
 
-                if (repeated_char(letters, p)) {
+                if (repeated_char(letters, p))
                         return false;
-                } else {
+                else
                         letters[i] = p;
-                }
-                ++phrase;
         }
 
         return true;
