fa9b967b12f5ba447a38319fe2bdd0180cdd27aa
Tiago Oliva <tiago.oliva.costa@gmail.com>
2021-09-20 19:32:34 -0300

    feat: isPangram for empty string

---
+++ haskell/pangram/src/Pangram.hs
@@ -0,0 +1,8 @@
+module Pangram (isPangram) where
+
+isPangram :: String -> Bool
+isPangram ""   = False
+isPangram text = error "You need to implement this function."
+--Ignore punctuation
+--Ignore numbers
+--Traverse string
